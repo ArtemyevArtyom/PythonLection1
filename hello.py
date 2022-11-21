fdff50735a3e3b14301be7b4870856e069b57459
ArtemyevArtyom <gof.ru@yandex.ru>
2022-11-21 14:58:17 +0400

    Python Lection 1

---
+++ hello.py
@@ -0,0 +1,16 @@
+value = None
+print(type(value))
+a = 123
+b = 1.23
+print(type(a))
+print(type(b))
+value = 12334
+print(type(value))
+
+s = 'hello world'
+
+print(s) # вывод строки
+print(a, b, s)
+
+list = ['1', '2', '3', 'hello']
+print(list)
